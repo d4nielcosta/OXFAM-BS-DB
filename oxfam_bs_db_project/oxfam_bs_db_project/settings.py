5adccbda75385c66ae6f21cae51cddb8f484bace
Daniel Costa <2087521d@student.gla.ac.uk>
2015-03-30 16:13:29 +0100

    Bootstrap offline

--- oxfam_bs_db_project/oxfam_bs_db_project/settings.py
+++ oxfam_bs_db_project/oxfam_bs_db_project/settings.py
@@ -78,12 +78,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (static, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 TEMPLATE_DIRS = (TEMPLATE_PATH,)
 
+#Static
+STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = (STATIC_PATH,)
--- oxfam_bs_db_project/volunteers/views.py
+++ oxfam_bs_db_project/volunteers/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from models import Volunteer
 
 
 def index(request):
+    context_dict = {}
 
-    return render(request, 'volunteers/index.html')
+    volunteers = Volunteer.objects.filter().order_by('-forename')
+    print volunteers
+    context_dict['volunteers'] = volunteers
+
+    return render(request, 'volunteers/index.html', context_dict)
 
 def profile(request, username):
     context_dict = {}
