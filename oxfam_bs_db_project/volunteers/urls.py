69d0564bc1d55c31a1c61d6ecb473a338c4a9385
Daniel Costa <2087521d@student.gla.ac.uk>
2015-03-30 14:06:48 +0100

    Created profile html

--- oxfam_bs_db_project/volunteers/urls.py
+++ oxfam_bs_db_project/volunteers/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, patterns
+
 import views
 
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.index, name='index'),
+                       url(r'^$', views.index, name='index'),
 
-    )
+                       #Profile Related
+                       url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
+
+
+                       )
--- oxfam_bs_db_project/volunteers/views.py
+++ oxfam_bs_db_project/volunteers/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 def index(request):
 
     return render(request, 'volunteers/index.html')
+
+def profile(request, username):
+    context_dict = {}
+
+    context_dict['user'] = username
+
+    return render (request, 'profile.html', context_dict)
