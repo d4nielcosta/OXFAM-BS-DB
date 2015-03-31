3f8a3c484c3f9025432e5362f1380768940e53eb
Daniel Costa <2087521d@student.gla.ac.uk>
2015-03-31 16:30:03 +0100

    Changed navbar colours

--- oxfam_bs_db_project/volunteers/views.py
+++ oxfam_bs_db_project/volunteers/views.py
@@ -15,29 +15,24 @@ def index(request):
 
 def profile(request, volunteer_id):
 
-
-
-
-
-
-    import smtplib
-
-    fromaddr = 'bitsnow1@aol.com'
-    toaddrs  = 'crazybioguy@gmail.com'
-    msg = 'A test email.'
-
-
-    # Credentials (if needed)
-    username = 'bitsnow1@aol.com'
-    password = '12ooottafagvSH'
-
-    # The actual mail send
-    server = smtplib.SMTP_SSL('smtp.aol.com:587')
-    server.starttls()
-    server.login(username,password)
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
-
+    # import smtplib
+    #
+    # fromaddr = 'bitsnow1@aol.com'
+    # toaddrs  = 'crazybioguy@gmail.com'
+    # msg = 'A test email.'
+    #
+    #
+    # # Credentials (if needed)
+    # username = 'bitsnow1@aol.com'
+    # password = '12ooottafagvSH'
+    #
+    # # The actual mail send
+    # server = smtplib.SMTP_SSL('smtp.aol.com:587')
+    # server.starttls()
+    # server.login(username,password)
+    # server.sendmail(fromaddr, toaddrs, msg)
+    # server.quit()
+    #
 
 
     context_dict = {}
